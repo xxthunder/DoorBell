97b01f5e0376e9acef9f2252ea2c018a4232f1a1
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2021-02-21 19:02:46 +0100

    Add and use library Ethernet 2.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
 #include <Arduino.h>
 #include <avr/sleep.h>
+#include <SPI.h>
+#include <Ethernet.h>
 
-// external interrupts
-#define INPUT_BUTTON_BELL1 PIND2
-#define INPUT_BUTTON_BELL2 PIND3
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 
-// Indicator leds for applied buttons
-#define OUTPUT_LED_BELL1 8
-#define OUTPUT_LED_BELL2 9
+// connected buttons
+#define INPUT_BUTTON1 PIND2
+#define INPUT_BUTTON2 PIND3
 
-boolean BUTTON_BELL1_APPLIED;
-boolean BUTTON_BELL2_APPLIED;
+// connected leds
+#define OUTPUT_LED1 8
+#define OUTPUT_LED2 9
+
+boolean BUTTON1_APPLIED;
+boolean BUTTON2_APPLIED;
 
 /**
  * Interrupt service routine for the first door bell
  */
 void isrBell1()
 {
-  BUTTON_BELL1_APPLIED = true;
+  BUTTON1_APPLIED = true;
 }
 
 /**
@@ -25,7 +31,7 @@ void isrBell1()
  */
 void isrBell2()
 {
-  BUTTON_BELL2_APPLIED = true;
+  BUTTON2_APPLIED = true;
 }
 
 /**
@@ -34,8 +40,8 @@ void isrBell2()
 void enterSleep(void)
 {
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(OUTPUT_LED_BELL1, LOW);
-  digitalWrite(OUTPUT_LED_BELL2, LOW);
+  digitalWrite(OUTPUT_LED1, LOW);
+  digitalWrite(OUTPUT_LED2, LOW);
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_enable();
   sleep_mode();
@@ -52,27 +58,52 @@ void setup()
   // configure LEDs
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  pinMode(OUTPUT_LED_BELL1, OUTPUT);
-  digitalWrite(OUTPUT_LED_BELL1, LOW);
-  pinMode(OUTPUT_LED_BELL2, OUTPUT);
-  digitalWrite(OUTPUT_LED_BELL2, LOW);
+  pinMode(OUTPUT_LED1, OUTPUT);
+  digitalWrite(OUTPUT_LED1, LOW);
+  pinMode(OUTPUT_LED2, OUTPUT);
+  digitalWrite(OUTPUT_LED2, LOW);
 
   // configure buttons
-  pinMode(INPUT_BUTTON_BELL1, INPUT_PULLUP);
-  pinMode(INPUT_BUTTON_BELL2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(INPUT_BUTTON_BELL1), isrBell1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(INPUT_BUTTON_BELL2), isrBell2, CHANGE);
-  delay(5000);
-  BUTTON_BELL1_APPLIED = false;
-  BUTTON_BELL2_APPLIED = false;
+  pinMode(INPUT_BUTTON1, INPUT_PULLUP);
+  pinMode(INPUT_BUTTON2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INPUT_BUTTON1), isrBell1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(INPUT_BUTTON2), isrBell2, CHANGE);
+  //delay(5000);
+  BUTTON1_APPLIED = false;
+  BUTTON2_APPLIED = false;
+
+  // configure ethernet
+  Serial.begin(9600);
+  if (Ethernet.begin(mac) == 0) 
+  {
+      Serial.println("Failed to configure Ethernet using DHCP");
+      // connection fails, stop program run. 
+      for(;;);
+  }
+
+// print out your local IP address
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) 
+  {
+    // print out four byte IP address
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print("."); 
+  }
+
+  Serial.println();
+}
+
+void handleButtonEvents()
+{
+  digitalWrite(OUTPUT_LED1, BUTTON1_APPLIED ? HIGH : LOW);
+  digitalWrite(OUTPUT_LED2, BUTTON2_APPLIED ? HIGH : LOW);
+  BUTTON1_APPLIED = false;
+  BUTTON2_APPLIED = false;
 }
 
 void loop()
 {
-  digitalWrite(OUTPUT_LED_BELL1, BUTTON_BELL1_APPLIED ? HIGH : LOW);
-  digitalWrite(OUTPUT_LED_BELL2, BUTTON_BELL2_APPLIED ? HIGH : LOW);
+  handleButtonEvents();
   delay(5000);
-  BUTTON_BELL1_APPLIED = false;
-  BUTTON_BELL2_APPLIED = false;
-  enterSleep();
+  //enterSleep();
 }
